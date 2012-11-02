9ebc66ab3f5cb7cc2317028a25848e776f9f976b
Jake Hodsgon <liveactiongamers@live.ca>
2012-11-02 13:58:48 -0700

    Init Commit

---
+++ mountsingapore/Block1.java
@@ -0,0 +1,12 @@
+package mod.mountsingapore;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.Material;
+
+public class Block1 extends Block {
+	public Block1(int i, int e){
+		super(i, e, Material.rock);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+	}
+}
---
+++ mountsingapore/mod_mountsingapore.java
@@ -0,0 +1,30 @@
+package mod.mountsingapore;
+
+import net.minecraft.src.Block;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+
+
+
+@Mod(modid ="MountSingapore", name = "tut", version = "0.0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class mod_mountsingapore {
+		public static Block Block1;
+		
+@Init
+public void load(FMLInitializationEvent event){
+	Block1 = new Block1(230, 0).setHardness(3F).setResistance(1.0F).setBlockName("TestBlock");
+	
+	GameRegistry.registerBlock(Block1);
+	
+	
+	LanguageRegistry.addName(Block1, "TempName");
+	
+	
+}
+}
