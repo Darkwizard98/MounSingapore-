9c83019d21cac8ac7821e5b0885dd5d909a86e90
Jake Hodsgon <liveactiongamers@live.ca>
2012-12-07 12:58:46 -0800

    Added the ability to config custom id's with presets.

--- mod/mountsingapore/mod_mountsingapore.java
+++ mod/mountsingapore/mod_mountsingapore.java
@@ -2,10 +2,14 @@ package mod.mountsingapore;
 
 import mountsingapore.Common.CommonProxy;
 import net.minecraft.src.Block;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
@@ -18,12 +22,25 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 public class mod_mountsingapore {
 		public static Block Block1;
 		
+		int Block1ID;
+		
 		@SidedProxy(clientSide="mountsingapore.Client.ClientProxy", serverSide="mountsingapore.common.ClientProxy")
 		public static CommonProxy proxy;
 		
+		
+		@PreInit
+		public void PreLoad(FMLPreInitializationEvent event){
+			
+			Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+			
+			config.load();
+			
+			Block1ID= config.getBlock("Block1ID", 230).getInt();
+			
+		}
 @Init
 public void load(FMLInitializationEvent event){
-	Block1 = new Block1(230, 0).setHardness(3F).setResistance(1.0F).setBlockName("TestBlock");
+	Block1 = new Block1(Block1ID, 0).setHardness(3F).setResistance(1.0F).setBlockName("TestBlock");
 	
 	GameRegistry.registerBlock(Block1);
 	
